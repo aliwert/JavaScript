4f30d727149a4769f54535d2438cdab10750f31c
aliwert <154356044+aliwert@users.noreply.github.com>
2024-02-15 17:43:27 +0300

    first commit

---
+++ game/game.js
@@ -0,0 +1,29 @@
+function rastgeleSayiUretmek() {
+  return Math.floor(Math.random() * 101);
+}
+
+function tahminEtme() {
+  const rastgeleSayi = rastgeleSayiUretmek();
+  let sans = 5;
+
+  while (sans > 0) {
+    const kullaniciTahmini = parseInt(
+      prompt("0-100 arasinda bir sayi tahmin et: ")
+    );
+
+    if (kullaniciTahmini === rastgeleSayi) {
+      alert(`Tebrikler dogru sayiyi ${5 - sans + 1}. denemede buldun`);
+      return;
+    } else if (kullaniciTahmini < rastgeleSayi) {
+      alert(`Daha yuksek bir sayi tahmin et. ${sans - 1} sansiniz kaldi`);
+    } else {
+      alert(`Daha dusuk bir sayi tahmin et. ${sans - 1} sansiniz kaldi`);
+    }
+
+    sans--;
+  }
+
+  alert(`Dogru sayi ${rastgeleSayi}.`);
+}
+tahminEtme();
+
